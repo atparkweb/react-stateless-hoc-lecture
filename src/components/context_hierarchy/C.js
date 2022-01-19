54c4ee37d03599cd687f0e88783e174dc70484d9
Andrew Park <atpark333@gmail.com>
2022-01-19 11:23:43 +0900

    docs: add comments to explain examples

--- src/components/context_hierarchy/C.js
+++ src/components/context_hierarchy/C.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ThemeContext from "../../contexts/ThemeContext";
 
+/* This component uses the render props pattern to 
+ * consume and use the color value from the ThemeContext
+ * */
+
 class C extends React.Component {
   render() {
     return (
--- src/components/context_hierarchy/D.js
+++ src/components/context_hierarchy/D.js
@@ -1,6 +1,12 @@
 import React from "react";
 import C from "./C";
 
+/* This component doesn't need the theme color context, but
+ * its child component (C) does. Rather than passing the data
+ * through props, we can use context provider in ancestor (A) to
+ * implicitly pass the prop through D to C.
+ * */
+
 class D extends React.Component {
   render() {
     return (
